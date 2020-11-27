0b8b84c2ed7d552a62ea2a3763430563bcfa75e5
shensq <shensq@umich.edu>
2020-11-27 14:51:25 -0500

    tokenizers.py: add new special tokens for scene and attributes.

--- dataloaders/tokenizers.py
+++ dataloaders/tokenizers.py
@@ -14,6 +14,10 @@ class VisualCometTokenizer(GPT2Tokenizer):
                  end_event="<|e_ev|>",
                  begin_place="<|b_pl|>",
                  end_place="<|e_pl|>",
+                 begin_scene="<|b_sc|>",
+                 end_scene="<|e_sc|>",
+                 begin_attribute="<|b_att|>",
+                 end_attribute="<|e_att|>",
                  begin_inferences={'before': "<|before|>", 'intent': "<|intent|>", 'after': "<|after|>"},
                  end_inference="<|e_in|>",
                  **kwargs):
@@ -33,19 +37,23 @@ class VisualCometTokenizer(GPT2Tokenizer):
         self.end_event = end_event
         self.begin_place = begin_place
         self.end_place = end_place
+        self.begin_scene = begin_scene
+        self.end_scene = end_scene
+        self.begin_attribute = begin_attribute
+        self.end_attribute = end_attribute
         self.begin_inferences = begin_inferences
         self.end_inference = end_inference
         self.det_tokens = ['<|det%d|>' % i for i in range(50)]
         self.add_special_tokens({
             "additional_special_tokens": [self.begin_img, self.end_img, self.begin_event, self.end_event,
-                                          self.begin_place, self.end_place, self.end_inference]
+                                          self.begin_place, self.end_place, self.end_inference, self.begin_scene, self.end_scene, self.begin_attribute, self.end_attribute]
                                          + list(self.begin_inferences.values()) + self.det_tokens
         })
 
     def decode(self, token_ids, skip_special_tokens=False, clean_up_tokenization_spaces=True):
         text = super().decode(token_ids, False, clean_up_tokenization_spaces)
         tokens2remove = [self.begin_img, self.end_img, self.begin_event, self.end_event,
-                         self.end_place, self.end_inference, self.unk_token] + list(self.begin_inferences.values())
+                         self.end_place, self.end_inference, self.unk_token, self.begin_scene, self.end_scene, self.begin_attribute, self.end_attribute] + list(self.begin_inferences.values())
         if skip_special_tokens:
             for t in tokens2remove:
                 text = text.replace(t, ' ')
